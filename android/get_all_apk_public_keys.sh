79fb7c68a2fe421d76f41b80d55e7b65a4f8eddd
YUAN Yu <yuan@archos.com>
2017-01-04 15:49:22 +0800

    add get_all_apk_public_keys.sh

---
+++ android/get_all_apk_public_keys.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+adb pull system/app/ app/
+adb pull system/priv-app/ priv-app/
+adb pull system/vendor/ vendor/
+
+mkdir public_keys
+for i in `find -name \*.apk`;do
+	# extract public key
+	tmpdir=$(mktemp -d)
+	unzip $i META-INF/CERT.RSA -d $tmpdir
+	filename=$(echo $i | sed -E 's#./(.*)#\1#g' | tr '/' '-')
+	filepath=public_keys/$filename
+	openssl pkcs7 -in $tmpdir/META-INF/CERT.RSA -print_certs -inform DER > $filepath
+
+	# classify them
+	if [ "$(find -name type\*)" ];then
+		maxnum=$(find -regex './type[0-9]' | sort | tail -n 1 | grep -Eo '[0-9]+')
+		match=no
+		for j in `find -regex './type[0-9]'`;do
+			if [ ! "$(diff $filepath $j)" ];then
+				match=$(echo $j | grep -Eo 'type[0-9]+')
+				break
+			fi
+		done
+		if [ "$match" == "no" ];then
+			maxnum=$((maxnum+1))
+			cp $filepath type$maxnum
+			echo $filename >> type${maxnum}.list
+		else
+			echo $filename >> ${match}.list
+		fi
+	else
+		cp $filepath type1
+		echo $filename >> type1.list
+	fi
+done
+
+rm -r app priv-app verdor
+#find -regex './type[0-9]+' -delete
